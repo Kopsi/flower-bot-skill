7e63bcaf94b5dec35397d4fe18182f4600c58ad0
Alexander Stahl <niuzicar@gmail.com>
2018-11-06 12:54:33 +0100

    fixed querys, added arduino data

--- __init__.py
+++ __init__.py
@@ -10,6 +10,8 @@
 from adapt.intent import IntentBuilder
 from mycroft.skills.core import MycroftSkill, intent_handler
 from mycroft.util.log import LOG
+from arduinoData import getCurrentSensorData
+
 
 # Each skill is contained within its own class, which inherits base methods
 # from the MycroftSkill class.  You extend this class as shown below.
@@ -20,32 +22,19 @@ class FlowerBotSkill(MycroftSkill):
     # The constructor of the skill, which calls MycroftSkill's constructor
     def __init__(self):
         super(FlowerBotSkill, self).__init__(name="FlowerBotSkill")
-        
-        # Initialize working variables used within the skill.
-        self.count = 0
-        # self.water = arduino.moist
-        # self.light = arduino.lux
-        self.water.time = 0
 
-    # The "handle_xxxx_intent" function is triggered by Mycroft when the
-    # skill's intent is matched.  The intent is defined by the IntentBuilder()
-    # pieces, and is triggered when the user's utterance matches the pattern
-    # defined by the keywords.  In this case, the match occurs when one word
-    # is found from each of the files:
-    #    vocab/en-us/Hello.voc
-    #    vocab/en-us/World.voc
-    # In this example that means it would match on utterances like:
-    #   'Hello world'
-    #   'Howdy you great big world'
-    #   'Greetings planet earth'
-    # @intent_handler(IntentBuilder("").require("Hello").require("World"))
-    # def handle_hello_world_intent(self, message):
-    #     # In this case, respond by simply speaking a canned response.
-    #     # Mycroft will randomly speak one of the lines from the file
-    #     #    dialogs/en-us/hello.world.dialog
-    #     self.speak_dialog("hello.world")
-    #
-    @intent_handler(IntentBuilder("").require("Count").require("Dir"))
+        # Initialize working variables used within the skill.
+        sensorData = getCurrentSensorData
+        self.count = 0
+        self.water = sensorData[1]
+        self.light = sensorData[2]
+        self.airPressure = sensorData[3]
+        self.airMoisture = sensorData[4]
+        self.temperature = sensorData[5]
+        self.waterTime = 0
+
+        @intent_handler(IntentBuilder("").require("Count").require("Dir"))
+
     def handle_count_intent(self, message):
         if message.data["Dir"] == "up":
             self.count += 1
@@ -53,31 +42,34 @@ class FlowerBotSkill(MycroftSkill):
             self.count -= 1
         self.speak_dialog("count.is.now", data={"count": self.count})
 
+        @intent_handler(IntentBuilder("").require("Flower"))
+        def handle_flower_intent(self, message):
+            if self.water < 350:
+                self.speak_dialog("need.water")
+            elif self.water > 900:
+                self.speak_dialog("too.much.water")
+            elif self.light < 50:
+                self.speak_dialog("need.light")
+            elif self.light > 400:
+                self.speak_dialog("too.much.light")
+            else:
+                self.speak_dialog("feeling.good")
 
-        # @intent_handler(IntentBuilder("").require("Flower"))
-        # def handle_flower_intent(self, message):
-        #     if  self.water < 350
-        #         self.speak_dialog("need.water")
-        #     elif self.water >900
-        #         self.speak_dialog("too.much.water")
-        #     elif self.light < 50
-        #         self.speak_dialog("need.light")
-        #     elif self.light > 400
-        #        self.speak_dialog("too.much.light")
-        #     else
-        #         self.speak_dialog("feeling.good")
-
-
-    @intent_handler(IntentBuilder("").require("Water.Check"))
-    def handle_water_check_intent(self, message):
-        self.speak_dialog("last.time.watered",  data={self.water.time})
+    # @intent_handler(IntentBuilder("").require("Water.Check"))
+    # def handle_water_check_intent(self, message):
+    #     if self.waterTime == 0:
+    #         self.speak_dialog("last.time.watered.today")
+    #     elif self.waterTime == 1:
+    #         self.speak_dialog("last.time.watered.yesterday")
+    #     elif self.waterTime > 1:
+    #         self.speak_dialog("last.time.watered", data ={"waterTime":self.waterTime},"time.ago")
 
     @intent_handler(IntentBuilder("").require("Need.Water")
     def handle_need_water_intent(self, message)
         self.speak_dialog("need.water")
 
     @intent_handler(IntentBuilder("").require("Edible"))
-    def handle_edible_intent(self,message):
+    def handle_edible_intent(self, message):
         self.speak_dialog("edible")
 
     # The "stop" method defines what Mycroft does when told to stop during
@@ -88,6 +80,7 @@ class FlowerBotSkill(MycroftSkill):
     #
     # def stop(self):
     #    return False
+
 
 # The "create_skill()" method is used to create an instance of the skill.
 # Note that it's outside the class itself.
