171d3b71d49d4f27579c0ca25ebaf7a6e8063f47
dfraas <davidfraas@web.de>
2019-01-22 13:52:52 +0100

    updated touch recognition

--- touchsense.py
+++ touchsense.py
@@ -1,3 +1,15 @@
+from websocket import create_connection
+uri = 'ws://localhost:8181/core'
+#ws = create_connection(uri)
+#print("Sending to " + uri + "...")
+
+message = '{"type": "mycroft.mic.listen", "data": {}}'
+#result = ws.send(message)
+#print("Receiving..." )
+#result = ws.recv()
+#print("Received '%s'" % result)
+#ws.close()
+
 import serial,time,os
 
 z1baudrate = 57600
@@ -6,33 +18,6 @@ z1port = '/dev/ttyACM0'  # set the correct port before run it
 z1serial = serial.Serial(port=z1port, baudrate=z1baudrate)
 z1serial.timeout = 2
 
-
-
-def getTouchValue():
-    if z1serial.is_open:
-        while True:
-            size = z1serial.inWaiting()
-            if size:
-                data = z1serial.readline(size)
-
-                if data.decode().startswith(" moist_"):
-
-                    pieces = data.split()
-                    if len(pieces) > 5:
-                        touchPieces = pieces[5].decode().split("_")
-                        if len(touchPieces) > 0:
-                            touchValueString = touchPieces[1]
-
-
-                    touchValue = float(touchValueString)
-
-                    return (touchValue)
-
-            else:
-                return 0
-            time.sleep(1)
-    else:
-        return 0
 
 lastValue = 0;
 isTouched = 0;
@@ -52,11 +37,14 @@ while True:
                     if(lastValue == 0):
                         lastValue = touchValue
                     else:
-                        if(lastValue-touchValue > 15):
+                        if(lastValue-touchValue > 5):
                             if(isTouched == 0):
                                 print("BERÜHRT")
                                 isTouched = 1;
-                                os.system("aplay /home/osboxes/mycroft-core/mimic/stop.wav")
+                                # ws = create_connection(uri)
+                                # result = ws.send(message)
+                                # ws.close()
+
                         else:
                             isTouched = 0;
                             lastValue = touchValue;
